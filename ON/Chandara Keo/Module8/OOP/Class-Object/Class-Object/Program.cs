67b4559800ccf55dee88f4590a4ffd9aae1b6bdb
Chandara Keo <keochandara@cityuniversity.edu>
2020-05-29 10:40:10 -0700

    HOP08

---
+++ ON/Chandara Keo/Module8/OOP/Class-Object/Class-Object/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Class_Object
+{
+    class Shapes
+    {
+        public double length;
+        public double width;
+        public double radius;
+        public double pi;
+        public double CalArea(double l, double w)
+        {
+            length = l;
+            width = w;
+            double Area = w * l;
+            return Area;
+        }
+        public double CalCircleArea(double r, double Pi)
+        {
+            radius = r;
+            pi = Pi;
+            double Area = Pi * r * r;
+            return Area;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double RecWidth = 7;
+            double RecLength = 8;
+            double SqWidth = 2;
+            double CirRadius = 3;
+            double ValuePi = 3.15;
+            Shapes Rectangle = new Shapes();
+            Shapes Square = new Shapes();
+            Shapes Circle = new Shapes();
+
+            Console.WriteLine("Area of Rectangle is: " + Rectangle.CalArea(RecLength, RecWidth));
+
+            Console.WriteLine("Area of Square is: " + Square.CalArea(SqWidth, SqWidth));
+
+            Console.WriteLine("Area of Circle is: " + Circle.CalCircleArea(CirRadius, ValuePi));
+        }
+    }
+}
