8b3ad48e3e5511b5d7060b71df4964e095d280f2
keochandara <63338740+keochandara@users.noreply.github.com>
2020-06-04 13:26:44 -0700

    HOP09

---
+++ ON/Chandara Keo/Module9/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Program
+    {
+        class Shapes //Create class named Shapes with below properties
+        {
+            public double length;
+            public double width;
+            public double radius;
+
+           
+        }
+        class Rectangle : Shapes
+        {
+            public void CalArea(double l, double w)
+            {
+                length = l;
+                width = w;
+                double RecArea = l * w;
+                Console.WriteLine("Rectangle length is: " + length);
+                Console.WriteLine("Rectangle width is: " + width);
+                Console.WriteLine("Rectangle Area is: " + RecArea);
+            }
+        }
+        class Square : Shapes
+        {
+            public void CalArea(double a)
+            {
+                length = a;
+                double SqArea = a * a;
+                Console.WriteLine("Square side length is: " + a);
+                Console.WriteLine("Square Area is: " + SqArea);
+
+            }
+
+        }
+        class Circle : Shapes
+        {
+            public void CalArea(double r)
+            {
+                radius = r;
+                double CirArea = 3.15 * r;
+                Console.WriteLine("Circle radius is: " + radius);
+                Console.WriteLine("Circal Area is: " + CirArea);
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Rectangle MyRec = new Rectangle();
+
+            Circle MyCir = new Circle();
+
+            Square MySq = new Square();
+
+            Console.WriteLine("Base length is " + MyRec.length);
+            Console.WriteLine("Base width is " + MyRec.width);
+            Console.WriteLine("Base radius is " + MyCir.radius);
+            Console.WriteLine("\n");
+
+
+
+            MyRec.CalArea(3, 4);
+            Console.WriteLine("\n");
+
+            MySq.CalArea(10);
+            Console.WriteLine("\n");
+
+            MyCir.CalArea(8);
+        }
+    }
+}
